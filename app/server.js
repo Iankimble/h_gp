5e33750bb9bb436ef5d77b7df2fae8c4171f3726
Ian Kimble <ian.kimble@eRT.com>
2020-01-13 22:45:08 -0500

    port2

--- app/server.js
+++ app/server.js
@@ -44,7 +44,7 @@ mongoose
   });
 
 // Port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
